b6d3f01096e12eeda6269d1389fb35b98d49d1e7
ayming <ayming1011@gmail.com>
2018-06-18 15:49:12 +0800

    added button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+import s from './Button.scss'
+
+class Button extends React.PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    color: PropTypes.oneOf(['primary', 'secondary']),
+    variant: PropTypes.oneOf(['contain', 'outline', 'text']),
+    children: PropTypes.node.isRequired,
+  }
+
+  static defaultProps = {
+    className: '',
+    color: 'primary',
+    variant: 'contain',
+  }
+
+  render() {
+    const { className, color, variant, children, ...props } = this.props
+    return (
+      <button
+        className={classnames(
+          {
+            [s.primary]: color === 'primary',
+            [s.secondary]: color === 'secondary',
+            [s.outline]: variant === 'outline',
+            [s.text]: variant === 'text',
+          },
+          s.wrapper,
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </button>
+    )
+  }
+}
+
+export default Button
--- src/routes/home/components/HomePanel/HomePanel.js
+++ src/routes/home/components/HomePanel/HomePanel.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import classnames from 'classnames'
+
+import Button from '../../../../components/Button'
+import s from './HomePanel.scss'
 
 class HomePanel extends React.PureComponent {
   render() {
     const { className } = this.props
-    return <div className={className}>Panel</div>
+    return (
+      <div className={className}>
+        <div className={s.container}>
+          <div className={s.body}>
+            <div className={s.top}>
+              <div className={s.description}>ROUTE/STOPS</div>
+              <Button className={s.addStop} variant="outline">
+                Add Stop
+              </Button>
+            </div>
+            <div className={s.inputList}>list</div>
+          </div>
+          <div className={classnames('justify', s.control)}>
+            <div className={s.controlItem}>
+              <Button className={s.button} color="secondary">
+                Reset
+              </Button>
+            </div>
+            <div className={s.controlItem}>
+              <Button className={s.button}>Submit</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
