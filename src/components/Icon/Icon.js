b9857f1f3396e9b2e46fe15736714cb769bb0b2b
ayming <ayming1011@gmail.com>
2018-06-18 17:36:57 +0800

    add icon component

---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,32 @@
+/**
+ * Fontawesome component
+ * @see https://github.com/FortAwesome/react-fontawesome
+ */
+import React from 'react'
+import PropTypes from 'prop-types'
+import fontawesome from '@fortawesome/fontawesome'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faMapMarkerAlt from '@fortawesome/fontawesome-free-solid/faMapMarkerAlt'
+import faCircle from '@fortawesome/fontawesome-free-regular/faCircle'
+import faDotCircle from '@fortawesome/fontawesome-free-regular/faDotCircle'
+
+fontawesome.library.add(faMapMarkerAlt, faCircle, faDotCircle)
+
+const TYPE = {
+  marker: ['fas', 'map-marker-alt'],
+  circle: ['far', 'circle'],
+  dot: ['far', 'dot-circle'],
+}
+
+class Icon extends React.PureComponent {
+  static propTypes = {
+    type: PropTypes.oneOf(Object.keys(TYPE)).isRequired,
+  }
+
+  render() {
+    const { type, ...props } = this.props
+    return <FontAwesomeIcon {...props} icon={TYPE[type]} />
+  }
+}
+
+export default Icon
